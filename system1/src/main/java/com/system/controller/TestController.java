d8b504972df1ad1bf14f5c80fe874192a870b0ff
1005046812 <1005046812@qq.com>
2020-12-07 21:51:36 +0800

    system中集成Mybatis

--- system1/src/main/java/com/system/controller/TestController.java
+++ system1/src/main/java/com/system/controller/TestController.java
@@ -1,12 +1,21 @@
 package com.system.controller;
 
+import com.system.domain.Test;
+import com.system.service.TestService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @RestController
 public class TestController {
+
+    @Resource
+    private TestService testService;
+
     @RequestMapping("/test")
-    public String test(){
-        return "success";
+    public List<Test> test(){
+        return testService.list();
     }
 }
---
+++ system1/src/main/java/com/system/mapper/TestMapper.java
@@ -0,0 +1,9 @@
+package com.system.mapper;
+
+import com.system.domain.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+    public List<Test> list();
+}
