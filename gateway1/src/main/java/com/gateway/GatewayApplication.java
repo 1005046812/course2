a7b579e8988790867170f474071a3dcbd22a89a6
1005046812 <1005046812@qq.com>
2020-12-08 17:42:43 +0800

    gateway路由的搭建

--- gateway1/src/main/java/com/gateway/GatewayApplication.java
+++ gateway1/src/main/java/com/gateway/GatewayApplication.java
@@ -1,6 +1,5 @@
-package com.system;
+package com.gateway;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -10,14 +9,13 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableEurekaClient
-@MapperScan("com.system.mapper")
-public class SystemApplication {
-    private static final Logger LOG= LoggerFactory.getLogger(SystemApplication.class);
+public class GatewayApplication {
+    private static final Logger LOG= LoggerFactory.getLogger(GatewayApplication.class);
     public static void main(String[] args) {
         //SpringApplication.run(CourseApplication.class, args);
-        SpringApplication app=new SpringApplication(SystemApplication.class);
+        SpringApplication app=new SpringApplication(GatewayApplication.class);
         Environment env=app.run(args).getEnvironment();
         LOG.info("启动成功！！");
-        LOG.info("System地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+        LOG.info("GateWay地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 }
